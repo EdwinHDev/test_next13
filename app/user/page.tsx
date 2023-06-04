3f2b4d47209436c451a384c0cdb347700f50fa33
Edwin Hernández <edwinhernandez.br@gmail.com>
2023-06-04 03:13:36 -0400

    pending fix ssr

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,7 +4,6 @@ import useSWR from "swr";
 import { IUser } from "@/interfaces/User";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Suspense } from "react";
 
 const fetcher = async (url: string): Promise<IUser[]> => {
   const res = await fetch(url, { cache: "no-store" });
@@ -18,43 +17,42 @@ const fetcher = async (url: string): Promise<IUser[]> => {
 export default function UserPage() {
 
   const router = useRouter();
-  const { data, error } = useSWR<IUser[]>('/api/user', fetcher, { refreshInterval: 1000, suspense: true });
+  const { data, error } = useSWR<IUser[]>('/api/user', fetcher, { refreshInterval: 1000 });
+
+  if (error) return <div>Error al cargar los datos</div>;
+  if (!data) return <div>Cargando...</div>;
 
   return (
     <div
       className="p-16 w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6"
     >
-      <Suspense fallback={<p className="text-slate-600 font-medium">Cargando usuarios...</p>}>
-        <div>
-          {
-            data?.map(user => (
+      {
+        data.map(user => (
+          <div
+            key={user.id}
+            className="p-4 border border-slate-300 flex gap-4 rounded-lg"
+          >
+            <div
+              className="rounded-3xl overflow-hidden w-40 h-40"
+            >
+              <Image width={160} height={160} src={user.avatar} alt={user.firstName} priority className="w-full h-full object-cover" />
+            </div>
+            <div>
+              <h2 className="text-slate-600 text-3xl">{user.firstName} {user.lastName}</h2>
               <div
-                key={user.id}
-                className="p-4 border border-slate-300 flex gap-4 rounded-lg"
+                className="flex gap-2 items-center mt-5"
               >
-                <div
-                  className="rounded-3xl overflow-hidden w-40 h-40"
-                >
-                  <Image width={160} height={160} src={user.avatar} alt={user.firstName} priority className="w-full h-full object-cover" />
-                </div>
-                <div>
-                  <h2 className="text-slate-600 text-3xl">{user.firstName} {user.lastName}</h2>
-                  <div
-                    className="flex gap-2 items-center mt-5"
-                  >
-                    <Image width={24} height={24} src="/phone.svg" alt="phone icon" />
-                    <p className="text-slate-500 text-xl">{user.phone}</p>
-                  </div>
-                  <button
-                    className="px-6 py-2 bg-slate-200 rounded-xl mt-7 text-slate-600 font-medium hover:bg-slate-300 transition-all duration-100"
-                    onClick={() => router.push(`user/${user.id}`)}
-                  >Ver Perfil</button>
-                </div>
+                <Image width={24} height={24} src="/phone.svg" alt="phone icon" />
+                <p className="text-slate-500 text-xl">{user.phone}</p>
               </div>
-            ))
-          }
-        </div>
-      </Suspense>
+              <button
+                className="px-6 py-2 bg-slate-200 rounded-xl mt-7 text-slate-600 font-medium hover:bg-slate-300 transition-all duration-100"
+                onClick={() => router.push(`user/${user.id}`)}
+              >Ver Perfil</button>
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
